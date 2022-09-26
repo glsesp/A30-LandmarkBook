d11434a366ea2d1b340e88adcc54559139656930
glsesp <103857972+glsesp@users.noreply.github.com>
2022-09-26 07:31:30 +0300

    Update LandmarkAdapter.java

--- app/src/main/java/com/atilsamancioglu/landmarkbook/LandmarkAdapter.java
+++ app/src/main/java/com/atilsamancioglu/landmarkbook/LandmarkAdapter.java
@@ -12,7 +12,12 @@ import com.atilsamancioglu.landmarkbook.databinding.RecyclerRowBinding;
 
 import java.util.ArrayList;
 
-public class LandmarkAdapter extends RecyclerView.Adapter<LandmarkAdapter.LandmarkHolder> {
+public class LandmarkAdapter extends RecyclerView.Adapter<LandmarkAdapter.LandmarkHolder> { //landmarkadapter'in içindeki landmark holder.
+    //inheritance yapıldı ki o sınıfın kodlarını kullanalım!
+    //viewHolder görünüm tutucudur(sınıf)
+    //methodlarını uygula(3 tane)
+    
+    
 
     ArrayList<Landmark> landmarkList;
 
@@ -23,12 +28,18 @@ public class LandmarkAdapter extends RecyclerView.Adapter<LandmarkAdapter.Landma
     @NonNull
     @Override
     public LandmarkHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //XML i bağlama işlemi binding burda olur
         RecyclerRowBinding recyclerRowBinding = RecyclerRowBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
         return new LandmarkHolder(recyclerRowBinding);
     }
 
     @Override
     public void onBindViewHolder(LandmarkAdapter.LandmarkHolder holder, int position) {
+        
+        //Oluşan viewHolder bağlanınca neler olacak!
+        
+        //layout içinde hangi verileri göstereceğiz!
+        
         holder.binding.recyclerViewTextView.setText(landmarkList.get(position).name);
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
@@ -44,11 +55,12 @@ public class LandmarkAdapter extends RecyclerView.Adapter<LandmarkAdapter.Landma
     }
 
     @Override
-    public int getItemCount() {
-        return landmarkList.size();
+    public int getItemCount() { //XML in kaç kez olusacagını gösterir
+        return landmarkList.size(); //listede ne kadarsa o kadar oluştur
     }
 
     public class LandmarkHolder extends RecyclerView.ViewHolder {
+        //Tek olayı Görünüm tutucudur.
 
         private RecyclerRowBinding binding;
 
