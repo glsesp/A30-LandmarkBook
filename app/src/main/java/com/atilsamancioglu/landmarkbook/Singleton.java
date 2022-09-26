6f37266b0028243a6ecf65ad72f53f58eef5dc9e
glsesp <103857972+glsesp@users.noreply.github.com>
2022-09-26 08:04:20 +0300

    Update Singleton.java

--- app/src/main/java/com/atilsamancioglu/landmarkbook/Singleton.java
+++ app/src/main/java/com/atilsamancioglu/landmarkbook/Singleton.java
@@ -3,12 +3,18 @@ package com.atilsamancioglu.landmarkbook;
 import android.graphics.Bitmap;
 
 public class Singleton {
+    //static değişkenleri kafaya göre kullanmak doğru değildir - herkes ulasabilr
+    //singleton-statik olarak erişebileceğimiz fakat tek bir objeye sahip bir sınıftır
+    //kontrol bende olur tek bır objesini oluşturuyor
 
-    private Landmark selectedLandmark;
-    private static Singleton singleton;
+    private Landmark selectedLandmark; // yollanan landmark
+    private static Singleton singleton;//static obje oluşur-kendim oluşturdum
 
     private Singleton() {
-
+        //constructorı private olmalıdır
+        //private yaparsak bu methoda sadece bu sınıf içinden ulasılır
+        //getter-setter lar oluştu
+        //ne zaman cagırırsam cagırıyım hep aynı obje döner
     }
 
     public Landmark getSelectedLandmark() {
@@ -18,12 +24,12 @@ public class Singleton {
     public void setChosenLandmark(Landmark selectedLandmark) {
         this.selectedLandmark = selectedLandmark;
     }
-
+//eğer önceden oluşmadıysa-ilk kez oluşuyosa
     public static Singleton getInstance() {
         if (singleton == null) {
             singleton = new Singleton();
         }
-
+//değislse return daha onceden olusan obje
         return singleton;
 
     }
